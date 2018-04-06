955807cbd128fe003bc9d686615aacefe796c059
JackyLSH <jackyleungsh@gmail.com>
2018-04-06 22:35:28 +0800

    Ping PHP server.

--- app/src/main/java/com/example/user/lavazone/HomeActivity.java
+++ app/src/main/java/com/example/user/lavazone/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.example.user.lavazone;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // Permitting Internet
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        Database db = new Database();
+        try {
+            Log.d("AppMsg", db.SendJSONRequest("get-item-list").toString());
+        }
+        catch(Exception e) {Log.d("AppMsg",e.toString());}
     }
 }
