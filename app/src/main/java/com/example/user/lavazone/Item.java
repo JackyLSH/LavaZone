24d186f6be31749d6eea03612f3d56ca0540c48c
KCDanny <chankwuncheung@hotmail.com>
2018-04-17 05:58:26 +0800

    Database finished

--- app/src/main/java/com/example/user/lavazone/Item.java
+++ app/src/main/java/com/example/user/lavazone/Item.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class Item {
     public int item_id = 0;
     public String name = "";
-    public String description = "";
+    public String item_description = "";
     public float price = 0;
     public int store_id = 0;
     public Date post_date = new Date();
