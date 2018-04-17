b69856ebf18347ba79552b4c1b8b27cdf0a7f4be
KCDanny <chankwuncheung@hotmail.com>
2018-04-17 20:24:15 +0800

    Database update

---
+++ app/src/main/java/com/example/user/lavazone/AdvancedSearchActivity.java
@@ -0,0 +1,49 @@
+package com.example.user.lavazone;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class AdvancedSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advance_search);
+
+        //on select navigation bar highlight
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavViewBar);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(1);
+        menuItem.setChecked(true);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.ic_home:
+                        Intent intent0 = new Intent(AdvancedSearchActivity.this, HomeActivity.class);
+                        startActivity(intent0);
+                        break;
+
+                    case R.id.ic_advasearch_search:
+                        Intent intent1 = new Intent(AdvancedSearchActivity.this, AdvancedSearchActivity.class);
+                        startActivity(intent1);
+                        break;
+
+                    case R.id.ic_cart:
+                        Intent intent2 = new Intent(AdvancedSearchActivity.this, CartActivity.class);
+                        startActivity(intent2);
+                        break;
+
+                }
+                return false;
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/user/lavazone/ItemDetailActivity.java
@@ -0,0 +1,48 @@
+package com.example.user.lavazone;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ItemDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_detail);
+
+        //on select navigation bar highlight
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavViewBar);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(1);
+        menuItem.setChecked(true);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.ic_home:
+                        Intent intent0 = new Intent(ItemDetailActivity.this, HomeActivity.class);
+                        startActivity(intent0);
+                        break;
+
+                    case R.id.ic_advasearch_search:
+                        Intent intent1 = new Intent(ItemDetailActivity.this, AdvancedSearchActivity.class);
+                        startActivity(intent1);
+                        break;
+
+                    case R.id.ic_cart:
+                        Intent intent2 = new Intent(ItemDetailActivity.this, CartActivity.class);
+                        startActivity(intent2);
+                        break;
+
+                }
+                return false;
+            }
+        });
+    }
+}
