6d6130858e70f99bae8a191501a6da689d08783f
JackyLSH <jackyleungsh@gmail.com>
2018-04-18 15:12:07 +0800

    Merged Homepage layout with topBar & bottomBar

--- app/src/main/java/com/example/user/lavazone/HomeActivity.java
+++ app/src/main/java/com/example/user/lavazone/HomeActivity.java
@@ -1,12 +1,17 @@
 package com.example.user.lavazone;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.StrictMode;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -14,6 +19,36 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //on select navigation bar highlisht
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavViewBar);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(0);
+        menuItem.setChecked(true);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.ic_home:
+                        Intent intent0 = new Intent(HomeActivity.this, HomeActivity.class);
+                        startActivity(intent0);
+                        break;
+
+                    case R.id.ic_advasearch_search:
+                        Intent intent1 = new Intent(HomeActivity.this, AdvancedSearchActivity.class);
+                        startActivity(intent1);
+                        break;
+
+                    case R.id.ic_cart:
+                        Intent intent2 = new Intent(HomeActivity.this, CartActivity.class);
+                        startActivity(intent2);
+                        break;
+
+                }
+                return false;
+            }
+        });
 
         // Permitting Internet
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
@@ -31,6 +66,7 @@ public class HomeActivity extends AppCompatActivity {
         }
     }
 
+    // Check network
     protected boolean isNetworkAvailable() {
         ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
