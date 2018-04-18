fd8adb56bfd88991192662a84b0412de34f966c9
cwying817 <cwy_ing@hotmail.com>
2018-04-18 22:30:52 +0800

    commit updated transaction and address page

--- app/src/main/java/com/example/user/lavazone/TransationActivity.java
+++ app/src/main/java/com/example/user/lavazone/TransationActivity.java
@@ -55,48 +55,48 @@ public class TransationActivity extends AppCompatActivity {
         return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 
-    public class BackgroundTask extends AsyncTask<Void, Void, String> {
-        String JSON_URL;
-
-        @Override
-        protected void onPreExecute() {
-            JSON_URL ="http://www2.comp.polyu.edu.hk/~15093307d/database.php";
-        }
-
-        @Override
-        protected String doInBackground(Void... params) {
-            try {
-                URL url = new URL(JSON_URL);
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                InputStream InputStream = httpURLConnection.getInputStream();
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(InputStream));
-                StringBuilder stringBuilder = new StringBuilder();
-                while ((JSON_STRING = bufferedReader.readLine())!=null) {
-                    stringBuilder.append(JSON_STRING + "\n");
-                }
-                bufferedReader.close();
-                InputStream.close();
-                httpURLConnection.disconnect();
-                String temp = stringBuilder.toString().trim();
-                String[] TempSplited  = temp.split(Pattern.quote("/"));
-
-                return stringBuilder.toString().trim();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onProgressUpdate(Void... values) {
-            super.onProgressUpdate(values);
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-        }
-
-    }
+//    public class BackgroundTask extends AsyncTask<Void, Void, String> {
+//        String JSON_URL;
+//
+//        @Override
+//        protected void onPreExecute() {
+//            JSON_URL ="http://www2.comp.polyu.edu.hk/~15093307d/database.php";
+//        }
+//
+//        @Override
+//            protected String doInBackground(Void... params) {
+//                try {
+//                    URL url = new URL(JSON_URL);
+//                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+//                    InputStream InputStream = httpURLConnection.getInputStream();
+//                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(InputStream));
+//                    StringBuilder stringBuilder = new StringBuilder();
+//                    while ((JSON_STRING = bufferedReader.readLine())!=null) {
+//                        stringBuilder.append(JSON_STRING + "\n");
+//                    }
+//                    bufferedReader.close();
+//                    InputStream.close();
+//                    httpURLConnection.disconnect();
+//                    String temp = stringBuilder.toString().trim();
+//                    String[] TempSplited  = temp.split(Pattern.quote("/"));
+//
+//                    return stringBuilder.toString().trim();
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//                return null;
+//        }
+//
+//        @Override
+//        protected void onProgressUpdate(Void... values) {
+//            super.onProgressUpdate(values);
+//        }
+//
+//        @Override
+//        protected void onPostExecute(String result) {
+//        }
+//
+//    }
 
 }
 
