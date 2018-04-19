707cfa6596dee15a16fd63e8d4abd9a7f99bd81f
JackyLSH <jackyleungsh@gmail.com>
2018-04-20 07:08:30 +0800

    Add CartItem

--- app/src/main/java/com/example/user/lavazone/CartActivity.java
+++ app/src/main/java/com/example/user/lavazone/CartActivity.java
@@ -5,8 +5,14 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Date;
 
 public class CartActivity extends AppCompatActivity {
 
@@ -14,6 +20,63 @@ public class CartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+
+        //receieve data
+        int item_id= getIntent().getIntExtra("item_id",0);
+        int store_id=getIntent().getIntExtra("store_id",0);
+        int quantity=getIntent().getIntExtra("qq",0);
+
+        Log.d("AppMsg","item_id: "+item_id);
+        Log.d("AppMsg","store_id: "+store_id);
+        Log.d("AppMsg","quantity: "+quantity);
+        //connect  to database
+        Database db = new Database();
+        Item item = null;
+        Store store = null;
+
+        float total = 0;
+
+        if (item_id != 0 && store_id != 0 && quantity != 0) {
+            try {
+                item = db.getItemDetailByID(item_id);
+            } catch (Exception e) {
+                Log.d("AppMsg", "DB error 1: " + e.getMessage());
+            }
+            try {
+                store = db.getStoreDetailByID(store_id);
+            } catch (Exception e) {
+                Log.d("AppMsg", "DB error 2: " + e.getMessage());
+            }
+            float price = 0;
+
+            TextView textView29 = (TextView) findViewById(R.id.textView29);
+            TextView textView26 = (TextView) findViewById(R.id.textView26);
+            TextView textView27 = (TextView) findViewById(R.id.textView27);
+            TextView textView25 = (TextView) findViewById(R.id.textView25);
+            TextView textView24 = (TextView) findViewById(R.id.textView24);
+            textView29.setText(item.name);
+            textView26.setText(String.valueOf(item.price));
+            textView27.setText(String.valueOf(quantity));
+            textView25.setText(store.name);
+            price=item.price*quantity;
+            total+=price;
+            textView24.setText(String.valueOf(price));
+        } else {
+            LinearLayout ll_left = (LinearLayout) findViewById(R.id.cartItemLeft);
+            LinearLayout ll_mid = (LinearLayout) findViewById(R.id.cartItemMid);
+            LinearLayout ll_right = (LinearLayout) findViewById(R.id.cartItemRight);
+            ll_left.setVisibility(View.GONE);
+            ll_mid.setVisibility(View.GONE);
+            ll_right.setVisibility(View.GONE);
+
+        }
+
+        //calculate total price
+        TextView textView15 = (TextView) findViewById(R.id.textView15);
+        textView15.setText(String.valueOf(total));
+        Log.d("AppMsg", "Value of total");
+
 
         //on select navigation bar highlight
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavViewBar);
